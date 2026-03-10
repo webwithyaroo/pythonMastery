cb3b7bbf972b9c6f38fc5366f09fd2d4a92a1384
yaroo <olawaleridollahi@gmail.com>
2026-03-10 16:37:41 +0100

    Added the dictionary comprehension logic

--- word_filter/num.py
+++ word_filter/num.py
@@ -5,3 +5,10 @@
 words = ["ai", "machine", "learning", "is", "powerful"]
 result = [word.upper() for word in words if len(word) > 3]
 print(result)
+
+
+
+numbers = [1,2,3,4]
+items_dict = {num:num*num for num in numbers}
+
+print(items_dict)
