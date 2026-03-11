a0c4f9a58d42ad4d984fb7b1c61e1ec2c54811e1
yaroo <olawaleridollahi@gmail.com>
2026-03-11 19:01:05 +0100

    Continued dict comprehension

--- word_filter/num.py
+++ word_filter/num.py
@@ -3,12 +3,18 @@
 
 
 words = ["ai", "machine", "learning", "is", "powerful"]
-result = [word.upper() for word in words if len(word) > 3]
+# result = [word.upper() for word in words if len(word) > 3]
+ #print(result)
+
+
+
+#numbers = [1,2,3,4]
+#item_dict = {num:num*num for num in numbers}
+#print(item_dict)
+
+
+
+numbers = [1,2,3,4,5,6]
+result = {num:num**2 for num in numbers if num % 2 == 0}
 print(result)
 
-
-
-numbers = [1,2,3,4]
-items_dict = {num:num*num for num in numbers}
-
-print(items_dict)
