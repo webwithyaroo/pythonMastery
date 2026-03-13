78f3a284b035ccdda24f374abbcb5f691696c392
yaroo <olawaleridollahi@gmail.com>
2026-03-13 22:43:39 +0100

    Added sort_by functionality

--- crashcourse/functions.py
+++ crashcourse/functions.py
@@ -1,7 +1,9 @@
         
-def counter_2 (text: str, min_length = 3, top_n=3) -> list[tuple[str,int]]:
+def analyze_words (text: str, min_length = 3, top_n=3, sort_by = "frequency") -> list[tuple[str,int]]:
     normalized_text = text.lower()
-    cleaned_text = "".join(char for char in normalized_text if text.isspace() or text.isalpha())
+    choice = sort_by.lower()
+    
+    cleaned_text = "".join(char for char in normalized_text if char.isspace() or char.isalpha())
     split_text = cleaned_text.split()
     
     word_check = [word for word in split_text if len(word) >= min_length ]
@@ -16,7 +18,12 @@ def counter_2 (text: str, min_length = 3, top_n=3) -> list[tuple[str,int]]:
             frequency[word]=1
     
     freq_result = frequency.items()
-    sort_item = sorted(freq_result, key=lambda item: item[1], reverse=True)
+
+    if choice == "frequency":
+        sort_item = sorted(freq_result, key=lambda item: item[1], reverse=True)
+    else:
+        sort_item = sorted(freq_result, key=lambda item: item[0])
+    
     top_items = sort_item[0:top_n]
     
     return top_items
@@ -24,8 +31,9 @@ def counter_2 (text: str, min_length = 3, top_n=3) -> list[tuple[str,int]]:
 if __name__ == "__main__":
     while True:
         user_input = input("Input: ")
+        sort_by = input("sort by WORD or FREQUENCY: ")
         if user_input:
-            result = counter_2(user_input, 2, 2)
+            result = analyze_words(user_input, 2, 2, sort_by)
             print(result)
             break
         print("Invalid input")
--- word_filter/num.py
+++ word_filter/num.py
@@ -2,6 +2,55 @@
 # This code demonstrates how to use list comprehension to filter and transform a list of words.
 
 
+
+"""
+    This module explains how a functions should handle multiple arguments
+"""
+
+def counter(text: str, min_word = 3)-> dict[str, int]:
+   
+    """ This is a function that takes 2 arguments, analyzes the text and return a dictionary showing the frequency of the words
+
+    Arg:
+        (text: str, min_word = 3):Takes in 2 arguments 
+    
+    Returns:
+        dict[str, int]:The expected result after analyses
+    """
+    normalized_text = text.lower()
+    cleaned_text = "".join(text for text in normalized_text if text.isspace() or text.isalpha())
+    split_text = cleaned_text.split()
+    
+    word_check = [word for word in split_text if len(word) > min_word ]
+    
+    frequency = {}
+    
+    for word in word_check:
+        if word in frequency:
+            frequency[word]+=1
+            
+        else:
+            frequency[word]=1
+    
+    
+    return frequency
+    
+    
+if __name__ == "__main__":
+    while True:
+        user_input = input("Input: ")
+        if user_input:
+            result = counter(user_input, 5)
+            print(result)
+            break
+        print("Invalid input")
+
+
+
+
+
+
+
 words = ["ai", "machine", "learning", "is", "powerful"]
 # result = [word.upper() for word in words if len(word) > 3]
  #print(result)
@@ -70,5 +119,3 @@ print(num_type)
 
 
 
-num_list = { num:num**2 for num in [1,2,3,4,5,6,7,8] if num <= 5}
-print(num_list)
