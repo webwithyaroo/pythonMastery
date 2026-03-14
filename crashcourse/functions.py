27355ac74c5b11ea9cdb9b42e528e086f60c77c5
yaroo <olawaleridollahi@gmail.com>
2026-03-14 23:05:20 +0100

    Tried fixing the function call errors

--- crashcourse/functions.py
+++ crashcourse/functions.py
@@ -1,13 +1,29 @@
         
-def analyze_words (text: str, min_length = 3, top_n=3, sort_by = "frequency") -> list[tuple[str,int]]:
+
+
+
+def clean_text(text: str, sort_by):
+    
     normalized_text = text.lower()
     choice = sort_by.lower()
-    
     cleaned_text = "".join(char for char in normalized_text if char.isspace() or char.isalpha())
-    split_text = cleaned_text.split()
     
+    split_text(cleaned_text)
+    
+    return cleaned_text
+
+def split_text(cleaned_text: str):
+    split_text = cleaned_text.split()
+    return split_text
+
+def filter_words(min_length: int):
     word_check = [word for word in split_text if len(word) >= min_length ]
-        
+    
+    count_frequency(word_check)
+    
+    return word_check
+
+def count_frequency(word_check: list):
     frequency = {}
     
     for word in word_check:
@@ -17,9 +33,13 @@ def analyze_words (text: str, min_length = 3, top_n=3, sort_by = "frequency") ->
         else:
             frequency[word]=1
     
-    freq_result = frequency.items()
+    sort_result(frequency)
+    return frequency
 
-    if choice == "frequency":
+def sort_result(frequency, sort_by, top_n):
+    freq_result = frequency.items()
+    
+    if sort_by == "frequency":
         sort_item = sorted(freq_result, key=lambda item: item[1], reverse=True)
     else:
         sort_item = sorted(freq_result, key=lambda item: item[0])
@@ -27,6 +47,21 @@ def analyze_words (text: str, min_length = 3, top_n=3, sort_by = "frequency") ->
     top_items = sort_item[0:top_n]
     
     return top_items
+
+
+
+
+
+def analyze_words (text: str, min_length = 3, top_n=3, sort_by = "frequency") -> list[tuple[str,int]]:
+
+    cln_text = clean_text(text, sort_by)
+    spt_text = split_text(cln_text)
+    fit_word = filter_words(spt_text)
+    result = sort_result(fit_word)
+  
+    return result
+
+    
         
 if __name__ == "__main__":
     while True:
@@ -37,3 +72,5 @@ if __name__ == "__main__":
             print(result)
             break
         print("Invalid input")
+        
+        
