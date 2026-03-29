68c352f05b69d0aaf8ec620ca3fa39437fbc8f9a
yaroo <olawaleridollahi@gmail.com>
2026-03-29 11:23:47 +0100

    feat: creat a file

---
+++ crashcourse/file_handling.py
@@ -0,0 +1,14 @@
+"""This module teaches file handling in python as the       process of 
+    creating, writing, updating and reading from a file, just like notebook
+    """
+    
+""" 3 operations involved are:
+    open
+    write
+    close \\ always close a file
+    """
+
+file = open("service.pdf", "w")
+write_file = file.write(" This is a service file ")
+print(write_file)
+file.close() # Always close file
