f41c8b06eab144f251c9030af24a90eb8368f6e5
yaroo <olawaleridollahi@gmail.com>
2026-03-30 16:56:54 +0100

    chore: append to the service file

--- crashcourse/file_handling.py
+++ crashcourse/file_handling.py
@@ -12,3 +12,13 @@ file = open("service.pdf", "w")
 write_file = file.write(" This is a service file ")
 file.close() # Always close file
 
+
+
+append_file = open("service.pdf", "a")
+append_file.write("\n New line added safely")
+append_file.close()
+
+read_file = open("service.pdf", "r")
+print(read_file.read())
+read_file.close()
+
